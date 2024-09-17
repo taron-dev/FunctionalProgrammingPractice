7635784aab63106b19497bc3674d6451853d9981
taron-dev <taron-dev@taron.sk>
2024-09-17 17:28:06 +0200

    Init commit

---
+++ src/main/java/sk/taron/sdaexamples/CarType.java
@@ -0,0 +1,5 @@
+package sk.taron.sdaexamples;
+
+public enum CarType {
+  COUPE, CABRIO, SEDAN, HATCHBACK
+}
---
+++ src/main/java/sk/taron/sdaexamples/Main.java
@@ -0,0 +1,27 @@
+package sk.taron.sdaexamples;
+
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Car car1 = new Car("Car A", "A sleek coupe", CarType.COUPE);
+    Car car2 = new Car("Car B", "A stylish cabrio", CarType.CABRIO);
+    Car car3 = new Car("Car C", "A modern sedan", CarType.SEDAN);
+    Car car4 = new Car("Car D", "A compact hatchback", CarType.HATCHBACK);
+    Car car5 = new Car("Car E", "Another sedan", CarType.SEDAN);
+
+    // Instantiate models for different manufacturers
+    Model model1 = new Model("Model X", 2020, Arrays.asList(car1, car2));  // Model with even year
+    Model model2 = new Model("Model Y", 2021, Arrays.asList(car3));  // Model with odd year
+    Model model3 = new Model("Model Z", 2018, Arrays.asList(car4, car5));  // Model with even year
+
+    // Instantiate manufacturers
+    Manufacturer manufacturer1 = new Manufacturer("Manufacturer One", 1915, Arrays.asList(model1));  // Odd year
+    Manufacturer manufacturer2 = new Manufacturer("Manufacturer Two", 1920, Arrays.asList(model2, model3));  // Even year
+    Manufacturer manufacturer3 = new Manufacturer("Manufacturer Three", 1890, Arrays.asList(model3));  // Even year
+
+    var manufacturers = Arrays.asList(manufacturer1, manufacturer2, manufacturer3);
+
+    System.out.println(manufacturers);
+  }
+}
